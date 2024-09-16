0a7659ef9ed942ee4282e5e99a89ed280338b471
afza shaikh <shaikhafza961@gmail.com>
2024-09-16 21:47:30 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+"use strict";
+let qoute = "A person who never made a mistake never tried anything new";
+let famousPerson = "Albert Einstien";
+let message = `${famousPerson} once said,${qoute}`;
+console.log(message);
---
+++ app.ts
@@ -0,0 +1,6 @@
+let qoute = "A person who never made a mistake never tried anything new";
+let famousPerson = "Albert Einstien";
+let message = `${famousPerson} once said,${qoute}`;
+console.log(message);
+
+
